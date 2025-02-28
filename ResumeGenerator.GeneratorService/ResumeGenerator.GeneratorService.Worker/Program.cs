54c6fb7f0a34a6ddf26c6e5bd12f0660a199d1f5
sug4chy <sb.9226981433@gmail.com>
2025-02-28 19:03:11 +0500

    Добавил директорию для Auth сервиса и Worker-а

---
+++ ResumeGenerator.GeneratorService/ResumeGenerator.GeneratorService.Worker/Program.cs
@@ -0,0 +1,7 @@
+using ResumeGenerator.GeneratorService.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
