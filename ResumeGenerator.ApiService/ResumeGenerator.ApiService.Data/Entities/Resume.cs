252625be0cf2f821d2860491cd0233e16312fb53
informant <artemiy.solodkin@mail.ru>
2025-03-03 23:09:32 +0500

    data layer done?

---
+++ ResumeGenerator.ApiService/ResumeGenerator.ApiService.Data/Entities/Resume.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeGenerator.ApiService.Data.Entities;
+
+public class Resume
+{
+    public required Guid ResumeId { get; set; }
+    
+    [MaxLength(50)]
+    public required string UserFirstName { get; set; }
+    
+    [MaxLength(50)]
+    public required string UserLastName { get; set; }
+    
+    [MaxLength(50)]
+    public required string UserPatronymic { get; set; }
+    
+    [MaxLength(20)]
+    public required string DesiredPosition { get; set; }
+    
+    public required string GitHubLink { get; set; }
+    
+    [MaxLength(50)]
+    public required string TelegramLink { get; set; }
+    
+    public required string Email { get; set; }
+    
+    [MaxLength(11)]
+    public required string PhoneNumber { get; set; }
+    
+    [MaxLength(80)]
+    public required string Education { get; set; }
+    
+    public required int ExperienceYears { get; set; }
+    
+    public required string HardSkills { get; set; }
+    public required string SoftSkills { get; set; }
+}
---
+++ ResumeGenerator.ApiService/ResumeGenerator.ApiService.Data/EntityConfigurations/ResumeEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ResumeGenerator.ApiService.Data.Entities;
+
+namespace ResumeGenerator.ApiService.Data.EntityConfigurations;
+
+public class ResumeEntityConfiguration: IEntityTypeConfiguration<Resume>
+{
+    public void Configure(EntityTypeBuilder<Resume> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(u => u.ResumeId);
+        builder.HasAlternateKey(u => u.PhoneNumber);
+    }
+}
