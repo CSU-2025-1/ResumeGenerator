19243f43fee69dcb3e6aae029b150cb8a29016ce
sug4chy <sb.9226981433@gmail.com>
2025-03-05 12:32:46 +0500

    Migration added

---
+++ ResumeGenerator.ApiService/ResumeGenerator.ApiService.Data/Migrations/20250305073157_Initial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeGenerator.ApiService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_first_name = table.Column<string>(type: "text", nullable: false),
+                    user_last_name = table.Column<string>(type: "text", nullable: false),
+                    user_patronymic = table.Column<string>(type: "text", nullable: false),
+                    desired_position = table.Column<string>(type: "text", nullable: false),
+                    github_link = table.Column<string>(type: "text", nullable: false),
+                    telegram_link = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    phone_number = table.Column<string>(type: "text", nullable: false),
+                    education = table.Column<string>(type: "text", nullable: false),
+                    experience_years = table.Column<int>(type: "integer", nullable: false),
+                    hard_skills = table.Column<string>(type: "text", nullable: false),
+                    soft_skills = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_phone_number",
+                table: "users",
+                column: "phone_number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
--- ResumeGenerator.ApiService/ResumeGenerator.ApiService.Web/Program.cs
+++ ResumeGenerator.ApiService/ResumeGenerator.ApiService.Web/Program.cs
@@ -1,12 +1,12 @@
+using ResumeGenerator.ApiService.Data.Extentions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDataLayerServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
