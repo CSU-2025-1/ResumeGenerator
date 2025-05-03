6f96e542d02c4071c4333a69a56ac7bb363210ff
sug4chy <sb.9226981433@gmail.com>
2025-05-03 17:14:43 +0500

    Fix

--- ResumeGenerator.Common/Contracts/CreateResumeCommand.cs
+++ ResumeGenerator.Common/Contracts/CreateResumeCommand.cs
@@ -1,18 +1,19 @@
 namespace ResumeGenerator.Common.Contracts;
 
-public sealed record CreateResumeCommand(
-    Guid ResumeId,
-    Guid UserId,
-    string FirstName,
-    string LastName,
-    string MiddleName,
-    string DesiredPosition,
-    string GitHubLink,
-    string TelegramLink,
-    string Email,
-    string PhoneNumber,
-    string Education,
-    int ExperienceYears,
-    string[] HardSkills,
-    string[] SoftSkills
-);
+public sealed record CreateResumeCommand
+{
+    public required Guid ResumeId { get; init; }
+    public required Guid UserId { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string MiddleName { get; init; }
+    public required string DesiredPosition { get; init; }
+    public required string GitHubLink { get; init; }
+    public required string TelegramLink { get; init; }
+    public required string Email { get; init; }
+    public required string PhoneNumber { get; init; }
+    public required string Education { get; init; }
+    public required int ExperienceYears { get; init; }
+    public required string[] HardSkills { get; init; }
+    public required string[] SoftSkills { get; init; }
+}
