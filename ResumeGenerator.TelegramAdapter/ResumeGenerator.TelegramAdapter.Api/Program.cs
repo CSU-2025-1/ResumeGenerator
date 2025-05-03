162d1cbf14f8780627d910f6409705ccad9bea75
sug4chy <sb.9226981433@gmail.com>
2025-05-03 13:45:36 +0500

    Прописал основную логику gRPC метода в сервисе

--- ResumeGenerator.TelegramAdapter/ResumeGenerator.TelegramAdapter.Api/Program.cs
+++ ResumeGenerator.TelegramAdapter/ResumeGenerator.TelegramAdapter.Api/Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(8080, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http1;
+    });
+
+    options.ListenLocalhost(8081, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
 
 var app = builder.Build();
 
@@ -8,5 +23,7 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.MapPost("/telegram-adapter/v1/updates", _ => Task.CompletedTask);
 
 app.Run();
---
+++ ResumeGenerator.TelegramAdapter/ResumeGenerator.TelegramAdapter.Core/Abstractions/ITelegramChatRepository.cs
@@ -0,0 +1,9 @@
+using CSharpFunctionalExtensions;
+using ResumeGenerator.TelegramAdapter.Core.Entities;
+
+namespace ResumeGenerator.TelegramAdapter.Core.Abstractions;
+
+public interface ITelegramChatRepository
+{
+    Task<Maybe<TelegramChat>> GetByUserIdAsync(Guid userId, CancellationToken ct = default);
+}
