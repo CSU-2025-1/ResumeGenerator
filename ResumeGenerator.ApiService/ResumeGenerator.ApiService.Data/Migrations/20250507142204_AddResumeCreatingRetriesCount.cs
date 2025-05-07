40eb441bf5316bfc13c814034608c9e1cfbd1c5a
sug4chy <sb.9226981433@gmail.com>
2025-05-07 19:22:36 +0500

    Сгенерировал миграцию с новым полем

---
+++ ResumeGenerator.ApiService/ResumeGenerator.ApiService.Data/Migrations/20250507142204_AddResumeCreatingRetriesCount.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeGenerator.ApiService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResumeCreatingRetriesCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "retry_count",
+                table: "resumes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "retry_count",
+                table: "resumes");
+        }
+    }
+}
