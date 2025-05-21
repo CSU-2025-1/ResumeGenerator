29e990e8e2b5054df68a7ce6a6645c786de555e1
sug4chy <sb.9226981433@gmail.com>
2025-05-21 13:10:22 +0500

    feat: добавил миграцию

---
+++ ResumeGenerator.ApiService/ResumeGenerator.ApiService.Data/Migrations/20250521081005_AddResumeName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResumeGenerator.ApiService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResumeName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "resume_name",
+                table: "resumes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "resume_name",
+                table: "resumes");
+        }
+    }
+}
